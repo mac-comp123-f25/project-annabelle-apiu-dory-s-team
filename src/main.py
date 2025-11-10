03831dd99e41fd39f76ab9a6adfc91f0f8e730da
aadachi27 <aadachi@macalester.edu>
2025-11-10 14:30:25 -0600

    draw face function

--- src/main.py
+++ src/main.py
@@ -2,3 +2,32 @@ from src.wa.member1 import *
 from src.wa.member2 import *
 from src.wa.member3 import *
 
+import turtle
+
+win = turtle.Screen()
+turt = turtle.Turtle()
+
+
+
+def draw_face(color):
+    # move turtle to position to start drawing face
+    turt.hideturtle()
+    turt.penup()
+    turt.right(90)
+    turt.forward(150)
+    turt.left(90)
+    turt.pendown()
+    # set turtle color to inputted color
+    turt.pencolor(color)
+    turt.fillcolor(color)
+    turt.begin_fill()
+    # draw face
+    for x in range(2):
+        turt.circle(120, 45)
+        turt.circle(200, 90)
+        turt.circle(120, 45)
+    turt.end_fill()
+
+draw_face("green")
+
+win.exitonclick()
